16fa6d9ccc3d1be90b3bc34d8a5d9368bde2c138
Adrian Patton <adrian.j.patton33@gmail.com>
2023-11-08 19:15:41 -0500

    added pages and routes but its not finished

--- client/src/Main.jsx
+++ client/src/Main.jsx
@@ -2,4 +2,39 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Error from './components/pages/error';
+import Login from './components/pages/login';
+import Logout from './components/pages/logout';
+import SignUp from './components/pages/SignUp';
+// Define the accessible routes, and which components respond to which URL
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Login />,
+      },
+      {
+        path: '/logout',
+        element: <Logout />,
+      },
+      {
+        path: '/signup',
+        element: <SignUp />,
+      },
+     
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />
+);
+
+// ReactDOM.createRoot(document.getElementById('root')).render(<App />);
--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,9 +1,11 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { Link, useLocation } from 'react-router-dom';
 
 
 function navBar() {
+  const currentPage = useLocation().pathname;
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
@@ -11,14 +13,33 @@ function navBar() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto-float-right ">
-            <Nav.Link href="#signUp">SignUp</Nav.Link>
-            <Nav.Link href="#login">login</Nav.Link>
-            <Nav.Link href="#logout">logout</Nav.Link>
-
-          </Nav>
+          <Link
+          to="/login"
+          // Check to see if the currentPage is `About`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
+          className={currentPage === './pages/login' ? 'nav-link active' : 'nav-link'}
+        >
+          Login
+        </Link>
+    
+        <Link
+          to="/logout"
+          // Check to see if the currentPage is `Blog`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
+          className={currentPage === '/logout' ? 'nav-link active' : 'nav-link'}
+        >
+          Logout
+        </Link>
+        <Link
+          to="/SignUp"
+          // Check to see if the currentPage is `Contact`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
+          className={currentPage === '/SignUp' ? 'nav-link active' : 'nav-link'}
+        >
+          SignUp
+        </Link>
+        </Nav>  
         </Navbar.Collapse>
       </Container>
     </Navbar>
+    
   );
 }
 
