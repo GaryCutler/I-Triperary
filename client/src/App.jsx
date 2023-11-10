f9aad2898351b5b9530ae2e81e8ec4af98607926
rgard630 <rgard630@gmail.com>
2023-11-09 21:47:51 -0500

    backgroung video

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,20 @@ import Nav from './components/Nav';
 import Createdtrips from './components/Createdtrips';
 import Destiantions from "./components/Destinations"
 import Grid from "./components/Grid"
+import './App.css'
 // Pass users array to the List component as a prop
 export default function App() {
   return (
-    <div>
+    <>
+    <video autoPlay muted loop id="bgVideo">
+      <source src="/Waterfall.mp4" type="video/mp4"/>
+      Your browser does not support HTML5 video.
+    </video>
+    <div style={{backgroundVideo: "url('/Waterfall.mp4')"}}>
       <Nav />
       <Createdtrips />
       < Grid />
     </div>
+    </>
   );
 }
